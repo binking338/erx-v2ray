f638c9f29b076d02fcb3ea3d956f74d496f0be4a
bin.wang <bin.wang@xiaobao100.com>
2020-04-30 17:20:29 +0800

    去掉链接地址

--- subscribe/index.sh
+++ subscribe/index.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
 rm -f /etc/v2ray/subs/*.json
-# ./sub-v2ray.sh https://www.mlycc.com/modules/servers/V2raySocks/subscribe/\?sid\=37637\&token\=uqIiT1DPLNzc
-./sub-v2ray.sh https://xxoo.men/link/SSyCYfJ8YOCetwrW?mu=2
 
 if [ "$#" = 0 ]; then
     echo "Enter EndPoint:"
